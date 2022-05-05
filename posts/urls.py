11c31cff07301a18fd795b6690f94b4cdccb5ec0
Yannick Rehberger <yr@ityreh.de>
2022-05-05 12:28:19 +0200

    feat(posts): add list and detail view

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # /posts/
+    path('', views.list, name='list'),
+    # /posts/<id>/
+    path('<int:post_id>/', views.read, name='read'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from posts.models import Post
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+
+def list(request):
+    latest_post_list = Post.objects.order_by('-pub_date')[:10]
+    output = ', '.join([p.post_title for p in latest_post_list])
+    return HttpResponse(output)
+
+def read(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
